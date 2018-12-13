91ed6ec5cb14131531fb42c48d5a45474f84a7c8
Xander Kaylan <xxk3990@rit.edu>
2018-12-12 20:02:44 -0500

    Added practice practical

--- fall2018sample/fall2018sample.js
+++ fall2018sample/fall2018sample.js
@@ -1,2 +1,14 @@
 /* Don't use <script> tags in a linked js file! */
 "use strict";
+$(".menubox").click(function () {
+    $(this).children(".submenu").slideToggle();
+    $(this).children(".submenu:hidden").show(); 
+    //what this does is it differentiates the two submenus so that clicking on the menu boxes will open their
+    //respective submenus and not both submenus
+});
+let file1 = "content1.txt";
+let name1 = $('#choose-content :selected').text();
+$("#content").text(name1);
+$("#content").load(file1);
+
+
